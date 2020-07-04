ebfa3471526dce8246d1c3bf8aa1f77e40fd79c5
Rathore-Aditya <42271261+Rathore-Aditya@users.noreply.github.com>
2020-07-04 14:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var body=document.getElementById("generator");
+//console.log(body);
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+// console.log(color1);
+function gradient()
+{
+    body.style.background="linear-gradient(to right"+","+color1.value+","+color2.value+")";
+    var h3=document.querySelector("h3");
+    h3.textContent=body.style.background + ";";
+}
+color1.addEventListener("input",gradient);
+color2.addEventListener("input",gradient);
+
